ab8c10b1175757c446a830c4095f76f2b4a77564
Lorenzo Carneli <lorenzo.carneli@epitech.eu>
2021-05-19 16:46:43 +0200

    feat: add register experimentation

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ function Home() {
                 <p>The first Cloud unsealing your Data</p>
             </div>
             <div className="home-btns">
-                <Link class="home-btn border-green" to="/signup">
+                <Link className="home-btn border-green" to="/signup">
                     Sign Up
                 </Link>
                 <div id="home-login-container">
                     <p>Already an account ?</p>
-                    <Link class="home-btn border-blue" to="/login">
+                    <Link className="home-btn border-blue" to="/login">
                         Log in
                     </Link>
                 </div>
--- src/components/forms/Signup.js
+++ src/components/forms/Signup.js
@@ -1,45 +1,61 @@
 import React, { useState } from 'react';
 import './Signup.css'
 
-export function Signup() {
+const bcrypt = require('bcryptjs');
+
+export function Signup({orbit_db}) {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [confPassword, setConfPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
 
     const emailChange = (event) => setEmail(event.target.value)
     const usernameChange = (event) => setUsername(event.target.value)
     const passwordChange = (event) => setPassword(event.target.value)
-    const confPasswordChange = (event) => setConfPassword(event.target.value)
+    const repeatPasswordChange = (event) => setRepeatPassword(event.target.value)
 
-    const displayInfos = () => {
-        console.log("email = " + email)
-        console.log("username = " + username)
-        console.log("password = " + password)
-        console.log("confPassword = " + confPassword)
+    const signUp = async (e) => {
+        e.preventDefault()
+
+        const db = await orbit_db.docs('/orbitdb/zdpuAwTKuRHqs7m5ecoD3yXJ6csM26MWmakdUJKCyKyDPyaDy/ipc.users',
+            {indexBy: 'email'});
+        const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync());
+
+        await db.load()
+        if (db.get(email) !== null) {
+            console.log("user exist!")
+            return
+        }
+        if (password !== repeatPassword) {
+            console.log("password doesn't matches with repeat password")
+            return
+        }
+        await db.put({_id: email, email: email, username: username, password: hashedPassword})
+        console.log('user created')
+        console.log(db.get(email))
     }
 
     return (
         <div className="signup-container">
-            <form className="signup-form" /*action="/dashboard" method="get"*/>
+            <form className="signup-form" onSubmit={(e) => signUp(e)}>
                 <div className="signup-form-field">
                     <label>email</label>
-                    <input type="email" id="email" name="user_email" onChange={emailChange}></input>
+                    <input type="email" id="email" name="user_email" onChange={emailChange}/>
                 </div>
                 <div className="signup-form-field">
                     <label>username</label>
-                    <input type="text" id="name" name="user_username" onChange={usernameChange}></input>
+                    <input type="text" id="name" name="user_username" onChange={usernameChange}/>
                 </div>
                 <div className="signup-form-field">
                     <label>password</label>
-                    <input type="text" id="name" name="user_password" onChange={passwordChange}></input>
+                    <input type="password" id="name" name="user_password" onChange={passwordChange}/>
                 </div>
                 <div className="signup-form-field">
                     <label>confirm-password</label>
-                    <input type="text" id="name" name="user_conf_password" onChange={confPasswordChange}></input>
+                    <input type="password" id="name" name="user_repeat_password" onChange={repeatPasswordChange}/>
                 </div>
                 <div className="signup-submit-btn">
-                    <button type="submit" onClick={displayInfos} >Sign Up</button>
+                    <button type="submit">Sign Up</button>
                 </div>
             </form>
         </div>
