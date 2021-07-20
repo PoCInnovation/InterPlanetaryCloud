094cdc2445cb13233cfd46f009747e7e2ce1f55b
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2021-07-20 13:34:33 +0200

    refa: database encapsulation

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,17 +3,67 @@ import IPFS from "ipfs-http-client";
 // @ts-ignore
 import OrbitDB from "orbit-db";
 
-export default class Database {
-    public orbit: any | null;
-    public ipfs: any | null;
+type OrbitStore = {
+    load: () => {},
+};
+
+type OrbitKVStore = OrbitStore & {
+    get: (key: string) => {},
+    set: (key: string, value: string) => {},
+};
+
+type OrbitDocStore = OrbitStore & {
+    get: (key: string) => {},
+    set: (key: string, value: any) => {},
+};
+
+export class KVStore {
+    private store: OrbitKVStore;
+
+    constructor(store: OrbitKVStore) {
+        this.store = store;
+    }
+
+    public async get(key: string) {
+        await this.store.load();
+        return this.store.get(key);
+    }
+
+    public async set(key: string, value: string) {
+        return this.store.set(key, value);
+    }
+};
+
+export class DocStore {
+    private store: OrbitDocStore;
+
+    constructor(store: OrbitDocStore) {
+        this.store = store;
+    }
+}
+
+export class Database {
+    private orbit: { docs: Function, keyvalue: Function } | null;
 
     constructor() {
-        this.ipfs = null;
         this.orbit = null;
     }
 
     public async init() {
-        this.ipfs = IPFS("http://localhost:5001");
-        this.orbit = await OrbitDB.createInstance(this.ipfs);
+        this.orbit = await OrbitDB.createInstance(IPFS("http://localhost:5001"));
+    }
+    
+    public async makeKVStore(address: string) {
+        if (this.orbit === null) {
+            throw "Database has not yet been initialised";
+        }
+        return new KVStore(await this.orbit.keyvalue(address));
+    }
+
+    public async makeDocStore(address: string) {
+        if (this.orbit === null) {
+            throw "Database has not yet been initialised";
+        }
+        return new DocStore(await this.orbit.docs(address));
     }
 };
