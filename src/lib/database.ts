ca1eef9c41a7666dcaafeb6650ddb6c5bee70241
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2021-07-20 10:28:31 +0200

    feat: database class

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,19 @@
-export default class Database {
-    constructor() {}
+// @ts-ignore
+import IPFS from "ipfs-http-client";
+// @ts-ignore
+import OrbitDB from "orbit-db";
 
-    public async init() {}
+export default class Database {
+    public orbit: any | null;
+    public ipfs: any | null;
+
+    constructor() {
+        this.ipfs = null;
+        this.orbit = null;
+    }
+
+    public async init() {
+        this.ipfs = IPFS("http://localhost:5001");
+        this.orbit = await OrbitDB.createInstance(this.ipfs);
+    }
 };
