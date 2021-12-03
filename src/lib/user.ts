ebad7b7f842ce6bda781bdf406ec59b8994aecb2
Lucas LOUIS <lucas.louis@epitech.eu>
2021-12-03 22:55:50 +0100

    :feat update user lib (load, upload, download) with new sdk

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,19 +1,18 @@
-import { message } from 'aleph-ts';
+import { accounts, post, store } from 'aleph-sdk-ts';
 
-import { accounts, post, store, DEFAULT_API_V2 } from 'aleph-sdk-ts';
-
-import CryptoJS from 'crypto-js';
+import { DEFAULT_API_V2 } from 'aleph-sdk-ts/global';
 
 import fileDownload from 'js-file-download';
+import { StorageEngine } from 'aleph-sdk-ts/messages/message';
+
+export function ArraybufferToString(ab: ArrayBuffer): string {
+	return new TextDecoder().decode(ab);
+}
 
 type IPCFile = {
 	name: string;
 	content: string;
 	created_at: number;
-};
-
-type UserData = {
-	files: IPCFile[];
 };
 
 type ResponseType = {
@@ -24,116 +23,141 @@ type ResponseType = {
 class Drive {
 	public files: IPCFile[];
 
+	public postsHash: string;
+
 	private readonly account: accounts.base.Account | undefined;
 
 	constructor(importedAccount: accounts.base.Account) {
 		this.files = [];
 		this.account = importedAccount;
+		this.postsHash = '';
 	}
 
-	/* public async load(): Promise<ResponseType> {
+	public async load(): Promise<ResponseType> {
 		try {
 			if (this.account) {
-				const userDataAddress = `ipc.user.${this.account.address}`;
-				let userData = await message.post.getPosts<UserData>(userDataAddress);
+				const userData = await post.Get({
+					APIServer: DEFAULT_API_V2,
+					types: '',
+					pagination: 200,
+					page: 1,
+					refs: [],
+					addresses: [this.account.address],
+					tags: [],
+					hashes: [],
+				});
 
-				if (userData.posts.length === 0) {
-					const newUserData = { files: this.files };
-
-					await message.post.submit(this.account.address, userDataAddress, newUserData, {
-						userAccount: this.account,
+				const postMessage = userData.posts.map((postContent) => {
+					const itemContent = JSON.parse(postContent.item_content);
+					if (itemContent.content.header === 'InterPlanetaryCloud2.0 Header') {
+						this.files = [];
+						this.postsHash = postContent.hash;
+						if (itemContent.content.files.length > 0) {
+							itemContent.content.files[0].map((file: IPCFile) => {
+								this.files.push(file);
+								return true;
+							});
+						}
+						return true;
+					}
+					return false;
+				});
+				if (postMessage.length !== 1) {
+					if (postMessage.length > 1) {
+						return { success: false, message: 'Too many post messages' };
+					}
+					console.log('Create Post Message');
+					const newPostPublishResponse = await post.Publish({
+						APIServer: DEFAULT_API_V2,
 						channel: 'TEST',
-						api_server: message.constants.DEFAULT_SERVER_V2,
+						inlineRequested: true,
+						storageEngine: StorageEngine.IPFS,
+						account: this.account,
+						postType: '',
+						content: {
+							header: 'InterPlanetaryCloud2.0 Header',
+							files: this.files,
+						},
 					});
-					userData = await message.post.getPosts<UserData>(userDataAddress);
-					this.files = userData.posts[userData.posts.length - 1].content.files;
-				} else {
-					this.files = userData.posts[userData.posts.length - 1].content.files;
+					this.postsHash = newPostPublishResponse.item_hash;
 				}
 				return { success: true, message: 'Drive loaded' };
 			}
+			return { success: false, message: 'Failed to load account' };
 		} catch (err) {
-			console.error(err);
+			console.log(err);
 			return { success: false, message: 'Failed to load drive' };
 		}
-		return { success: false, message: 'Failed to load drive' };
-	} */
-
-	public async load(): Promise<ResponseType> {
-		return { success: true, message: 'Drive loaded' };
-		// try {
-		// 	if (this.account) {
-		// 		const userAddress = this.account.address;
-		//
-		// 		const userData = await post.Get({
-		// 			APIServer: DEFAULT_API_V2,
-		// 			types: "",
-		// 			pagination: 200,
-		// 			page: 1,
-		// 			refs: [],
-		// 			addresses: [userAddress],
-		// 			tags: [],
-		// 			hashes: []
-		// 		});
-		// 		this.files = userData.posts[userData.posts.length - 1].content;
-		// 		return { success: true, message: 'Drive loaded' };
-		// 	}
-		// } catch (err) {
-		// 	console.error(err);
-		// 	return { success: false, message: 'Failed to load drive' };
-		// }
-		// return { success: false, message: 'Failed to load drive' };
 	}
 
 	public async upload(file: IPCFile): Promise<ResponseType> {
-		return { success: true, message: 'File uploaded' };
-		// try {
-		// 	if (this.account) {
-		// 		const userDataAddress = `ipc.user.${this.account.address}`;
-		// 		const userData = await message.post.getPosts<UserData>(userDataAddress);
-		//
-		// 		if (userData.posts.length > 0) {
-		// 			const encryptedFile = {
-		// 				name: file.name,
-		// 				content: CryptoJS.AES.encrypt(file.content, this.account.private_key).toString(),
-		// 				created_at: file.created_at,
-		// 			};
-		// 			this.files.push(encryptedFile);
-		//
-		// 			const newUserData: UserData = { files: this.files };
-		// 			await message.post.submit(this.account.address, 'amend', newUserData, {
-		// 				userAccount: this.account,
-		// 				channel: 'TEST',
-		// 				api_server: message.constants.DEFAULT_SERVER_V2,
-		// 				ref: userData.posts[userData.posts.length - 1].item_hash,
-		// 			});
-		// 			return { success: true, message: 'File uploaded' };
-		// 		}
-		// 	}
-		// } catch (err) {
-		// 	console.error(err);
-		// 	return { success: false, message: 'Failed to upload file' };
-		// }
-		// return { success: false, message: 'Failed to upload file' };
+		try {
+			if (this.account) {
+				const encryptedContentFile = CryptoJS.AES.encrypt(file.content, 'test').toString();
+
+				const newStoreFile = new File([encryptedContentFile], file.name, {
+					type: 'text/plain',
+				});
+
+				const fileHashPublishStore = await store.Publish({
+					channel: 'TEST',
+					account: this.account,
+					fileObject: newStoreFile,
+					storageEngine: StorageEngine.IPFS,
+					APIServer: DEFAULT_API_V2,
+				});
+
+				const newFile: IPCFile = {
+					name: file.name,
+					content: fileHashPublishStore,
+					created_at: file.created_at,
+				};
+
+				this.files.push(newFile);
+				await post.Publish({
+					APIServer: DEFAULT_API_V2,
+					channel: 'TEST',
+					inlineRequested: true,
+					storageEngine: StorageEngine.IPFS,
+					account: this.account,
+					postType: 'amend',
+					content: {
+						header: 'InterPlanetaryCloud2.0 Header',
+						files: [this.files],
+					},
+					ref: this.postsHash,
+				});
+
+				return { success: true, message: 'File uploaded' };
+			}
+			return { success: false, message: 'Failed to load account' };
+		} catch (err) {
+			console.log(err);
+			return { success: false, message: 'Failed to upload the file' };
+		}
 	}
 
 	public async download(file: IPCFile): Promise<ResponseType> {
-		// try {
-		// 	if (this.account) {
-		// 		const decryptedFile: IPCFile = {
-		// 			name: file.name,
-		// 			content: CryptoJS.AES.decrypt(file.content, this.account.private_key).toString(CryptoJS.enc.Utf8),
-		// 			created_at: file.created_at,
-		// 		};
-		// 		const blob = new Blob([decryptedFile.content]);
-		// 		fileDownload(blob, decryptedFile.name);
-		// 		return { success: true, message: 'File downloaded' };
-		// 	}
-		// } catch (err) {
-		// 	console.error(err);
-		// 	return { success: false, message: 'Failed to download file' };
-		// }
-		return { success: false, message: 'Failed to download file' };
+		try {
+			if (this.account) {
+				const storeFile = await store.Get({
+					APIServer: DEFAULT_API_V2,
+					fileHash: file.content,
+				});
+
+				const decryptedContentFile = CryptoJS.AES.decrypt(ArraybufferToString(storeFile), 'test').toString(
+					CryptoJS.enc.Utf8,
+				);
+
+				const blob = new Blob([decryptedContentFile]);
+				fileDownload(blob, file.name);
+				return { success: true, message: 'File downloaded' };
+			}
+			return { success: false, message: 'Failed to load account' };
+		} catch (err) {
+			console.log(err);
+			return { success: false, message: 'Failed to download the file' };
+		}
 	}
 }
 
