760cae5c09e18646103570094d5d7b279ce12f80
Lucas LOUIS <lucas.louis@epitech.eu>
2022-03-12 17:24:48 +0100

    feat: split dahsboard tests in two files (front/ back)

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -14,55 +14,6 @@ describe('Create account for DashboardView tests', () => {
 	});
 });
 
-describe('Good front for DashboardView', () => {
-	it('Go to dashboard view', () => {
-		cy.visit('http://localhost:3000/login');
-		cy.wait(1000);
-		cy.get('#ipc-loginView-text-area').click().type(dashboardSpecMnemonic);
-		cy.get('#ipc-loginView-credentials-button').click();
-	});
-
-	it('Good title', () => {
-		cy.get('#ipc-sideBar-title').should('contain', 'Inter Planetary Cloud');
-	});
-
-	it('Good name for upload button', () => {
-		cy.get('#ipc-dashboardView-drawer-button').click({ force: true });
-		cy.get('#ipc-upload-button').should('contain', 'Upload a file');
-	});
-});
-
-describe('Good Modal Front for DashboardView', () => {
-	it('Go to upload modal into dashboard view', () => {
-		cy.visit('http://localhost:3000/login');
-		cy.wait(1000);
-		cy.get('#ipc-loginView-text-area').click().type(dashboardSpecMnemonic);
-		cy.get('#ipc-loginView-credentials-button').click().wait(3000);
-		cy.get('#ipc-dashboardView-drawer-button').click({ force: true });
-		cy.get('#ipc-upload-button').click();
-	});
-
-	it('Good header', () => {
-		cy.get('header').should('contain', 'Upload a file');
-	});
-
-	it('Good number of buttons', () => {
-		cy.get('button').should('have.length', 5);
-	});
-
-	it('Good number of input', () => {
-		cy.get('input[type=file]').should('have.length', 1);
-	});
-
-	it('Good name for upload a file button', () => {
-		cy.get('#ipc-dashboardView-upload-file-modal-button').should('contain', 'Upload file');
-	});
-
-	it('Good name for close button', () => {
-		cy.get('#ipc-modal-close-button').should('contain', 'Close');
-	});
-});
-
 describe('Upload a file modal for DashboardView', () => {
 	const fixtureFile = 'upload_test_file.txt';
 
@@ -72,7 +23,7 @@ describe('Upload a file modal for DashboardView', () => {
 		cy.get('#ipc-loginView-text-area').click().type(dashboardSpecMnemonic);
 		cy.get('#ipc-loginView-credentials-button').click().wait(3000);
 		cy.get('#ipc-dashboardView-drawer-button').click({ force: true });
-		cy.get('#ipc-upload-button').click();
+		cy.get('#ipc-upload-button').click().wait(2500);
 	});
 
 	it('Good number of buttons after upload', () => {
@@ -94,7 +45,7 @@ describe('Download a file for DashboardView', () => {
 		cy.wait(1000);
 		cy.get('#ipc-loginView-text-area').click().type(dashboardSpecMnemonic);
 		cy.get('#ipc-loginView-credentials-button').click();
-		cy.wait(3000);
+		cy.wait(2500);
 		cy.get('#ipc-dashboardView-download-button').click();
 		cy.wait(3000);
 	});
