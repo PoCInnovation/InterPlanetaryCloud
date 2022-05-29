b4941305de4a58a5666c86b4cd93a66210541ecc
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-05-29 21:34:35 +0200

    fix: change function structure
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

--- src/components/ButtonIpCloud.tsx
+++ src/components/ButtonIpCloud.tsx
@@ -2,22 +2,19 @@ import { Link as RouteLink } from 'react-router-dom';
 
 import { Button, Link } from '@chakra-ui/react';
 
-const ButtonIpCloud = (): JSX.Element => {
-
-    return (
-        <>
-            <Link as={RouteLink} to="/dashboard" w="100%" id="ipc-homeView-login-button">
-                <Button
-                    variant="inline"
-                    w="100%"
-                    mb="16px"
-                    id="ipc-dashboardView-upload-file-modal-button"
-                >
-                    Inter Planetary Cloud
-                </Button>
-            </Link>
-        </>
-    )
-}
+const ButtonIpCloud = (): JSX.Element => (
+    <>
+        <Link as={RouteLink} to="/dashboard" w="100%" id="ipc-homeView-login-button">
+            <Button
+                variant="inline"
+                w="100%"
+                mb="16px"
+                id="ipc-dashboardView-upload-file-modal-button"
+            >
+                Inter Planetary Cloud
+            </Button>
+        </Link>
+    </>
+)
 
 export default ButtonIpCloud;
--- src/components/ButtonIpComputing.tsx
+++ src/components/ButtonIpComputing.tsx
@@ -2,22 +2,19 @@ import { Link as RouteLink } from 'react-router-dom';
 
 import { Button, Link } from '@chakra-ui/react';
 
-const ButtonIpComputing = (): JSX.Element => {
-
-    return (
-        <>
-            <Link as={RouteLink} to="/computing" w="100%" id="ipc-homeView-login-button">
-                <Button
-                    variant="inline"
-                    w="100%"
-                    mb="16px"
-                    id="ipc-dashboardView-upload-file-modal-button"
-                >
-                    Inter Planetary Computing
-                </Button>
-            </Link>
-        </>
-    )
-}
+const ButtonIpComputing = (): JSX.Element => (
+    <>
+        <Link as={RouteLink} to="/computing" w="100%" id="ipc-homeView-login-button">
+            <Button
+                variant="inline"
+                w="100%"
+                mb="16px"
+                id="ipc-dashboardView-upload-file-modal-button"
+            >
+                Inter Planetary Computing
+            </Button>
+        </Link>
+    </>
+)
 
 export default ButtonIpComputing;
