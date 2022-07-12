33a9a3e1dddaedd8c3980a84612f3b7bac6e4763
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-07-12 12:28:07 +0200

    feat: Context menu handling on file right click

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text, VStack } from '@chakra-ui/react';
+import { MouseEvent, useState } from 'react';
 
 import type { IPCFile } from 'types/types';
 
@@ -7,27 +8,38 @@ type FileCardProps = {
 	children: JSX.Element;
 };
 
-const FileCard = ({ file, children }: FileCardProps): JSX.Element => (
-	<Box
-		p="16px"
-		bg="white"
-		w="100%"
-		boxShadow="0px 2px 3px 3px rgb(240, 240, 240)"
-		borderRadius="4px"
-		border="1px solid rgb(200, 200, 200)"
-		mb="8px"
-		display="flex"
-		justifyContent="space-between"
-	>
-		<VStack w="100%" justify="space-between" align="center">
-			<Text fontWeight="500" isTruncated maxW="100%">
-				{file.name}
-			</Text>
-			<Flex w="100%" justify="space-between" align="center">
-				{children}
-			</Flex>
-		</VStack>
-	</Box>
-);
+const FileCard = ({ file, children }: FileCardProps): JSX.Element => {
+	const [open, setOpen] = useState(false);
+
+	const handleClick = (e: MouseEvent): void => {
+		e.preventDefault();
+		if (open) console.log(`Opening menu for ${file.hash}`);
+		else console.log(`Closing menu for ${file.hash}`);
+		setOpen(!open);
+	};
+	return (
+		<Box
+			p="16px"
+			bg="white"
+			w="100%"
+			boxShadow="0px 2px 3px 3px rgb(240, 240, 240)"
+			borderRadius="4px"
+			border="1px solid rgb(200, 200, 200)"
+			mb="8px"
+			display="flex"
+			justifyContent="space-between"
+			onContextMenu={(e) => handleClick(e)}
+		>
+			<VStack w="100%" justify="space-between" align="center">
+				<Text fontWeight="500" isTruncated maxW="100%">
+					{file.name}
+				</Text>
+				<Flex w="100%" justify="space-between" align="center">
+					{children}
+				</Flex>
+			</VStack>
+		</Box>
+	);
+};
 
 export default FileCard;
