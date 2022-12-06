97af5958b82374d4db8fb4fb475d8a9a1a9522ee
Mehdi Djendar <mehdi.djendar@epitech.eu>
2022-12-06 19:09:59 +0100

    [ADD] logs drawer

--- src/components/file/DetailsFile.tsx
+++ src/components/file/DetailsFile.tsx
@@ -1,11 +1,41 @@
-import { HStack, Icon, Text, useBreakpointValue, useDisclosure } from '@chakra-ui/react';
+import {
+	Box,
+	DrawerHeader,
+	Drawer,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerBody,
+	HStack,
+	Icon,
+	Text,
+	useBreakpointValue,
+	CloseButton,
+	useDisclosure,
+} from '@chakra-ui/react';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
-
+import React, { useState } from 'react';
+import colors from 'theme/foundations/colors';
 import { IPCFile } from 'types/types';
+import { useUserContext } from 'contexts/user';
+import type { IPCContact } from 'types/types';
 
 const DetailsFile = ({ file }: { file: IPCFile }): JSX.Element => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const isDrawer = useBreakpointValue({ base: true, sm: false }) || false;
+	const { user } = useUserContext();
+	const [showShared, setShow] = useState(false);
+	const toggleFct = () => {
+		setShow(!showShared);
+	};
+
+	const list: { name: string }[] = [];
+	user.contact.contacts.forEach((contact: IPCContact) => {
+		contact.files.forEach((contactFile: IPCFile) => {
+			if (contactFile.hash === file.hash) {
+				list.push({ name: contact.name });
+			}
+		});
+	});
 
 	return (
 		<HStack
@@ -35,8 +65,104 @@ const DetailsFile = ({ file }: { file: IPCFile }): JSX.Element => {
 					fontWeight: '500',
 				}}
 			>
-				Details
+				Détail
 			</Text>
+			<Drawer onClose={onClose} isOpen={isOpen} size={'sm'}>
+				<DrawerOverlay />
+				<DrawerContent>
+					<HStack>
+						<DrawerHeader fontSize={'20px'}> {file.name} </DrawerHeader>
+						<Box display="flex" alignSelf={'right'}>
+							<CloseButton size="lg" />
+						</Box>
+					</HStack>
+					<Box
+						display="flex"
+						mt="2"
+						alignSelf={'center'}
+						margin="-7px 0px 25px 0px"
+						w={{ base: '400px' }}
+						h="5px"
+						bg={`linear-gradient(135deg, ${colors.blue[900]} 0%, ${colors.red[900]} 40%)`}
+						borderRadius="16px"
+					/>
+					<DrawerBody>
+						<Text fontSize="l">
+							Create at Nov.{' '}
+							{`${new Date(file.createdAt).toString().substring(4, 15).slice(0, 3)}
+						/${new Date(file.createdAt).toString().substring(4, 15).slice(3, 6)}
+						/${new Date(file.createdAt).toString().substring(4, 15).slice(6)}`}
+						</Text>
+						<Text
+							fontSize={{ base: '16px', sm: '20px' }}
+							fontWeight="bold"
+							bgGradient={`linear-gradient(90deg, ${colors.blue[700]} 0%, ${colors.red[700]} 100%)`}
+							bgClip="text"
+							id="line-title"
+						>
+							By !LUK
+						</Text>
+						<br />
+						<Text fontSize="l"> Update at Nov. 25th 2022 </Text>
+						<Text
+							fontSize={{ base: '16px', sm: '20px' }}
+							fontWeight="bold"
+							bgGradient={`linear-gradient(90deg, ${colors.blue[700]} 0%, ${colors.red[700]} 100%)`}
+							bgClip="text"
+							id="logs-title"
+						>
+							By Mehdi
+						</Text>
+						<br />
+						<Text fontSize="l" marginBottom="10px">
+							{' '}
+							Size of : <b>{file.size / 1000}ko</b>
+						</Text>
+						<Text fontSize="l" marginBottom="10px">
+							{' '}
+							Hash of ipfs: <b>{file.hash}</b>
+						</Text>
+						<Text fontSize="l" marginBottom="10px">
+							{' '}
+							Type : PDF file{' '}
+						</Text>
+						<br />
+						<HStack onClick={toggleFct}>
+							<Box
+								display="flex"
+								alignItems="baseline"
+								backgroundColor={colors.blue[100]}
+								borderRadius="6px"
+								padding={'10px 10px 10px 10px'}
+							>
+								<Text fontSize={{ base: '16px', sm: '17px' }}>This file is shared with:</Text>
+								<Text
+									fontSize={{ base: '16px', sm: '17px' }}
+									fontWeight="bold"
+									bgGradient={`linear-gradient(90deg, ${colors.blue[700]} 0%, ${colors.red[700]} 100%)`}
+									bgClip="text"
+									id="nbr-title"
+								>
+									4 people
+								</Text>
+							</Box>
+						</HStack>
+						{showShared ? list.map((item) => <Text>{item.name}</Text>) : null}
+						<br />
+						<Box
+							display="flex"
+							mt="2"
+							alignSelf={'center'}
+							margin="-7px 0px 25px 0px"
+							w={{ base: '400px' }}
+							h="5px"
+							bg={`linear-gradient(135deg, ${colors.blue[900]} 0%, ${colors.red[900]} 40%)`}
+							borderRadius="16px"
+						/>
+						<Text fontSize="l"> History : </Text>
+					</DrawerBody>
+				</DrawerContent>
+			</Drawer>
 		</HStack>
 	);
 };
--- src/components/file/LogsFile.tsx
+++ src/components/file/LogsFile.tsx
@@ -1,6 +1,10 @@
-import { Text, VStack } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 import colors from 'theme/foundations/colors';
 import React, { useState } from "react";
+import { HStack, Icon, Text, useBreakpointValue, useDisclosure, Drawer } from '@chakra-ui/react';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+
+import { IPCFile } from 'types/types';
 
 type LogsInfo = {
 	title: string;
@@ -20,35 +24,69 @@ const Logs = ({ title }: LogsInfo): JSX.Element => {
             return(<button onClick={toggleFct}> Show logs </button>)
 
         if (showCardLogs === 1) {
-            return (
-                <>
-                    <button onClick={toggleFct}> Hide logs</button>
-                    <div className="cardContainer" style={{
-                        width:"400px",
-                        height:"600px",
-                        overflow: "hidden",
-                        boxShadow: "15px 15px 15px -7px",
-                        textAlign:"center",
-                        color: "purple",
-                        backgroundColor:"white",
-                        position:"absolute",
-                        right:"-815px",
-                        padding: "30px 30px 30px 30px",
-                        borderRadius: "20px",
-                        margin: "130px 20px 0px 0px"}}>
-                        <VStack textAlign="center" w="100%">
-                            <Text
-	        	    			fontSize={{ base: '16px', sm: '24px' }}
-	        	    			fontWeight="bold"
-	        	    			bgGradient={`linear-gradient(90deg, ${colors.blue[700]} 0%, ${colors.red[700]} 100%)`}
-	        	    			bgClip="text"
-	        	    			id="logs-title">
-	        	    		    Logs
-	        	    		</Text>
-	        	    	</VStack>
-                    </div>
-                </>
-            )
+            <button onClick={toggleFct}> Hide logs</button>
+            const { isOpen, onOpen, onClose } = useDisclosure();
+	        const isDrawer = useBreakpointValue({ base: true, sm: false }) || false;
+
+	        return (
+	        	<HStack
+	        		spacing={isDrawer ? '24px' : '12px'}
+	        		p="8px 12px"
+	        		borderRadius="8px"
+	        		role="group"
+	        		onClick={onOpen}
+	        		w="100%"
+	        		cursor="pointer"
+	        		_hover={{
+	        			bg: 'blue.100',
+	        		}}
+	        	>
+	        		<Icon
+	        			as={AiOutlineInfoCircle}
+	        			_groupHover={{ color: 'red.800' }}
+	        			w={isDrawer ? '24px' : '20px'}
+	        			h={isDrawer ? '24px' : '20px'}
+	        		/>
+	        		<Text
+	        			fontSize="16px"
+	        			fontWeight="400"
+	        			_groupHover={{
+	        				color: 'red.800',
+	        				fontWeight: '500',
+	        			}}
+	        		>
+	        			Details
+	        		</Text>
+	        	</HStack>
+	        );
+            // return (
+            //     <>
+            //         <div className="cardContainer" style={{
+            //             width:"400px",
+            //             height:"600px",
+            //             overflow: "hidden",
+            //             boxShadow: "15px 15px 15px -7px",
+            //             textAlign:"center",
+            //             color: "purple",
+            //             backgroundColor:"white",
+            //             position:"absolute",
+            //             right:"-815px",
+            //             padding: "30px 30px 30px 30px",
+            //             borderRadius: "20px",
+            //             margin: "130px 20px 0px 0px"}}>
+            //             <VStack textAlign="center" w="100%">
+            //                 <Text
+	        // 	    			fontSize={{ base: '16px', sm: '24px' }}
+	        // 	    			fontWeight="bold"
+	        // 	    			bgGradient={`linear-gradient(90deg, ${colors.blue[700]} 0%, ${colors.red[700]} 100%)`}
+	        // 	    			bgClip="text"
+	        // 	    			id="logs-title">
+	        // 	    		    Logs
+	        // 	    		</Text>
+	        // 	    	</VStack>
+            //         </div>
+            //     </>
+            // )
         }
 		return <></>
 }
