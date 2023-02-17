b471581eac0b7e7de416f58ea4ff7d5baf4ce8eb
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:45:23 +0100

    lint: lint the code

--- src/components/dashboardPage/FolderOptions.tsx
+++ src/components/dashboardPage/FolderOptions.tsx
@@ -1,20 +1,38 @@
-import { Box, Drawer, DrawerBody, DrawerContent, DrawerOverlay, PopoverTrigger, Popover, useDisclosure, VStack, PopoverContent, Portal, PopoverBody } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { IPCFolder } from "../../types/types";
-import MoveFolder from "../folder/MoveFolder";
-import DeleteFolder from "../folder/DeleteFolder";
-import { useConfigContext } from "../../contexts/config";
+import {
+	Box,
+	Drawer,
+	DrawerBody,
+	DrawerContent,
+	DrawerOverlay,
+	PopoverTrigger,
+	Popover,
+	useDisclosure,
+	VStack,
+	PopoverContent,
+	Portal,
+	PopoverBody,
+} from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { IPCFolder } from '../../types/types';
+import MoveFolder from '../folder/MoveFolder';
+import DeleteFolder from '../folder/DeleteFolder';
+import { useConfigContext } from '../../contexts/config';
 
-const FolderOptionsContent = ({folder}: {folder: IPCFolder}): JSX.Element => (
+const FolderOptionsContent = ({ folder }: { folder: IPCFolder }): JSX.Element => (
 	<>
 		<MoveFolder folder={folder} />
 		<DeleteFolder folder={folder} />
 	</>
-)
+);
 
-const FolderOptionsPopover = ({folder, clickPosition, popoverOpeningToggle, popoverOpeningHandler}: {
+const FolderOptionsPopover = ({
+	folder,
+	clickPosition,
+	popoverOpeningToggle,
+	popoverOpeningHandler,
+}: {
 	folder: IPCFolder;
-	clickPosition: {x: number, y: number};
+	clickPosition: { x: number; y: number };
 	popoverOpeningToggle: boolean;
 	popoverOpeningHandler: () => void;
 }): JSX.Element => {
@@ -31,8 +49,7 @@ const FolderOptionsPopover = ({folder, clickPosition, popoverOpeningToggle, popo
 	return (
 		<Popover placement="right-start" isOpen={isOpen} onClose={() => onClose()}>
 			<PopoverTrigger>
-				<Box position="absolute" w="1px" h="1px" bg="transparent" top={clickPosition.y}
-				     left={clickPosition.x} />
+				<Box position="fixed" w="1px" h="1px" bg="transparent" top={clickPosition.y} left={clickPosition.x} />
 			</PopoverTrigger>
 			<Portal>
 				<PopoverContent
@@ -52,20 +69,28 @@ const FolderOptionsPopover = ({folder, clickPosition, popoverOpeningToggle, popo
 				</PopoverContent>
 			</Portal>
 		</Popover>
-	)
+	);
 };
 
-const FolderOptionsDrawer = ({ folder, isOpen, onClose }: {folder: IPCFolder, isOpen: boolean, onClose: () => void}): JSX.Element => (
-		<Drawer isOpen={isOpen} onClose={onClose} placement="bottom">
-			<DrawerOverlay />
-			<DrawerContent borderRadius="16px 16px 0px 0px">
-				<DrawerBody px="16px" py="32px">
-					<VStack w="100%" spacing="12px">
-							<FolderOptionsContent folder={folder} />
-						</VStack>
-					</DrawerBody>
-				</DrawerContent>
-		</Drawer>
-	)
+const FolderOptionsDrawer = ({
+	folder,
+	isOpen,
+	onClose,
+}: {
+	folder: IPCFolder;
+	isOpen: boolean;
+	onClose: () => void;
+}): JSX.Element => (
+	<Drawer isOpen={isOpen} onClose={onClose} placement="bottom">
+		<DrawerOverlay />
+		<DrawerContent borderRadius="16px 16px 0px 0px">
+			<DrawerBody px="16px" py="32px">
+				<VStack w="100%" spacing="12px">
+					<FolderOptionsContent folder={folder} />
+				</VStack>
+			</DrawerBody>
+		</DrawerContent>
+	</Drawer>
+);
 
 export { FolderOptionsPopover, FolderOptionsDrawer };
