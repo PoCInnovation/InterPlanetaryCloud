4ee4dd55e61fb07fc6f264c765f42e87443d33fc
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:46:49 +0100

    feat: add content to shared page

--- pages/drive/shared.tsx
+++ pages/drive/shared.tsx
@@ -1,9 +1,18 @@
 import { Text, VStack } from '@chakra-ui/react';
+import DriveCards from '../../src/components/cards/DriveCards';
+import Navigation from '../../src/components/navigation/Navigation';
+import { useDriveContext } from '../../src/contexts/drive';
 
-const Shared = (): JSX.Element => (
-	<VStack>
-		<Text>Shared page</Text>
-	</VStack>
-);
+const Shared = (): JSX.Element => {
+	const { sharedFiles } = useDriveContext();
+
+	return (
+		<Navigation>
+			<VStack w="100%" id="test" spacing="16px">
+				<DriveCards files={sharedFiles.filter((elem) => !elem.deletedAt)} />
+			</VStack>
+		</Navigation>
+	);
+};
 
 export default Shared;
