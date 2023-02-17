cae821956e1a395b3e0c1c1afe68a872142a1b6c
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:42:53 +0100

    feat: add sub routes management for navbar

--- src/components/navigation/NavbarItem.tsx
+++ src/components/navigation/NavbarItem.tsx
@@ -13,17 +13,17 @@ const NavbarItem = ({ item }: { item: NavbarType }): JSX.Element => {
 			w="100%"
 			cursor="pointer"
 			borderRadius="8px"
-			bg={router.pathname.includes(item.url) ? 'blue.100' : ''}
+			bg={router.pathname === item.url ? 'blue.100' : ''}
 			onClick={() => router.push(item.url)}
 			_hover={{
 				bg: 'blue.50',
 			}}
 		>
-			<Icon as={item.icon} w="20px" h="20px" color={router.pathname.includes(item.url) ? 'red.800' : '#000000'} />
+			<Icon as={item.icon} w="20px" h="20px" color={router.pathname === item.url ? 'red.800' : '#000000'} />
 			<Text
 				size="lg"
-				fontWeight={router.pathname.includes(item.url) ? '500' : '400'}
-				color={router.pathname.includes(item.url) ? 'red.800' : '#000000'}
+				fontWeight={router.pathname === item.url ? '500' : '400'}
+				color={router.pathname === item.url ? 'red.800' : '#000000'}
 			>
 				{item.label}
 			</Text>
