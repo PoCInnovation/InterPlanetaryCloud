635d65b008fe4f3a6e8863f74c74595028e5fdd2
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:35:23 +0100

    feat: add content to contact page

--- pages/drive/contacts.tsx
+++ pages/drive/contacts.tsx
@@ -1,9 +1,27 @@
-import { Text, VStack } from '@chakra-ui/react';
+import { HStack, Text, VStack } from '@chakra-ui/react';
+import Button from 'components/Button';
+import { BsPlusLg } from 'react-icons/bs';
+import colors from 'theme/foundations/colors';
+import { useDriveContext } from '../../src/contexts/drive';
+import Navigation from '../../src/components/navigation/Navigation';
+import DriveCards from '../../src/components/cards/DriveCards';
+import LabelBadge from '../../src/components/LabelBadge';
+import AddContact from "../../src/components/contact/AddContact";
 
-const Contacts = (): JSX.Element => (
-	<VStack>
-		<Text>Contacts page</Text>
-	</VStack>
-);
+const Contacts = (): JSX.Element => {
+	const { contacts } = useDriveContext();
+
+	return (
+		<Navigation>
+			<VStack w="100%" id="test" spacing="48px" align="start">
+				<HStack spacing="48px">
+					<LabelBadge label="Contacts" />
+					<AddContact />
+				</HStack>
+				<DriveCards contacts={contacts} />
+			</VStack>
+		</Navigation>
+	);
+};
 
 export default Contacts;
