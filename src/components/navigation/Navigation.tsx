fd25544a92790d9c15f0788de4a5db998b670a62
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:44:15 +0100

    fix: update padding

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,16 +2,27 @@ import { useBreakpointValue, VStack } from '@chakra-ui/react';
 
 import { useConfigContext } from 'contexts/config';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { ResponsiveBar } from './ResponsiveBar';
+import { useUserContext } from '../../contexts/user';
 
 const Navigation = ({ children }: { children: JSX.Element }): JSX.Element => {
 	const { config } = useConfigContext();
+	const { user } = useUserContext();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!user) {
+			router.push('/');
+		}
+	}, []);
 
 	return (
 		<VStack
 			align="start"
 			bg={config?.theme ?? 'white'}
-			m={{ base: '96px 64px 32px 64px', lg: '96px 64px 32px 364px' }}
+			m={{ base: '128px 64px 32px 64px', lg: '128px 64px 32px 364px' }}
 		>
 			<ResponsiveBar />
 			{children}
