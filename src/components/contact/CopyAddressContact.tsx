d4f0505c3ccf83c65eec2e7e036bc62c085f0175
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:49:51 +0100

    feat: add contact options popover, drawer & content

---
+++ src/components/contact/CopyAddressContact.tsx
@@ -0,0 +1,54 @@
+import { HStack, Icon, Text, useBreakpointValue, useToast } from '@chakra-ui/react';
+import { FiDownload } from 'react-icons/fi';
+
+import { useUserContext } from 'contexts/user';
+
+import type { IPCContact, IPCFile } from 'types/types';
+import { BsClipboard } from 'react-icons/bs';
+
+type DownloadFileProps = {
+	contact: IPCContact;
+	onClose: () => void;
+};
+
+const CopyAddressContact = ({ contact, onClose }: DownloadFileProps): JSX.Element => {
+	const isDrawer = useBreakpointValue({ base: true, sm: false }) || false;
+
+	return (
+		<HStack
+			spacing={isDrawer ? '24px' : '12px'}
+			p="8px 12px"
+			borderRadius="8px"
+			role="group"
+			onClick={() => {
+				navigator.clipboard.writeText(contact.address);
+				onClose();
+			}}
+			w="100%"
+			cursor="pointer"
+			id="ipc-dashboard-download-button"
+			_hover={{
+				bg: 'blue.100',
+			}}
+		>
+			<Icon
+				as={BsClipboard}
+				_groupHover={{ color: 'red.800' }}
+				w={isDrawer ? '24px' : '20px'}
+				h={isDrawer ? '24px' : '20px'}
+			/>
+			<Text
+				fontSize="16px"
+				fontWeight="400"
+				_groupHover={{
+					color: 'red.800',
+					fontWeight: '500',
+				}}
+			>
+				Copy the address
+			</Text>
+		</HStack>
+	);
+};
+
+export default CopyAddressContact;
