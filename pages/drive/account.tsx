e68b9d04aa212cb59b41d14b32d8edab0d9ab545
Florian Lauch <florian.lauch@epitech.eu>
2023-03-02 22:16:52 +0100

    feat(config): light / dark theme functional

--- pages/drive/account.tsx
+++ pages/drive/account.tsx
@@ -1,14 +1,17 @@
-import { VStack } from '@chakra-ui/react';
+import {VStack} from '@chakra-ui/react';
 
 import Navigation from 'components/navigation/Navigation';
 import LabelBadge from "components/LabelBadge";
 import AccountCard from 'components/account/AccountCard';
+import React from "react";
+import ConfigCard from "../../src/components/account/ConfigCard";
 
 const Account = (): JSX.Element => (
 	<Navigation>
 		<VStack w="100%" spacing="48px" align="start">
 			<LabelBadge label="Account" />
 			<AccountCard />
+			<ConfigCard />
 		</VStack>
 	</Navigation>
 );
