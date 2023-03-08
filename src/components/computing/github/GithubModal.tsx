0d407fc7f8f4ca2b3954e3c3abbb232596976d37
Lucas LOUIS <lucas.louis@epitech.eu>
2023-03-08 22:53:19 +0100

    feat: improve github repositories select with search input

--- src/components/computing/github/GithubModal.tsx
+++ src/components/computing/github/GithubModal.tsx
@@ -1,17 +1,19 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import { HStack, Select, useToast, Text, VStack } from '@chakra-ui/react';
+import { HStack, useToast, Text, VStack, Input, Skeleton } from '@chakra-ui/react';
 import axios from 'axios';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 import Modal from 'components/Modal';
 import Button from 'components/Button';
+import Tooltip from 'components/Tooltip';
+import Card from 'components/cards/Card';
 
 import { useUserContext } from 'contexts/user';
 import { useDriveContext } from 'contexts/drive';
 
 import { GitHubRepository, IPCProgram } from 'types/types';
-import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+
 import CustomProgram from '../CustomProgram';
 
 const GithubModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }): JSX.Element => {
@@ -27,8 +29,8 @@ const GithubModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void
 	const [customName, setCustomName] = useState<string>('');
 	const [customEntrypoint, setCustomEntrypoint] = useState<string>('');
 	const [repositories, setRepositories] = useState<GitHubRepository[]>([]);
-	const [selectedPage, setSelectedPage] = useState(1);
-	const [hasMore, setHasMore] = useState(false);
+	const [repositoriesLoading, setRepositoriesLoading] = useState(false);
+	const [searchInput, setSearchInput] = useState(selectedRepository);
 
 	useEffect(() => {
 		(async () => {
@@ -39,10 +41,10 @@ const GithubModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void
 	}, []);
 
 	useEffect(() => {
-		(async () => {
-			await getRepositories();
-		})();
-	}, [selectedPage]);
+		const foundRepository = repositories.find((repository) => repository.html_url === selectedRepository);
+		if (foundRepository) setSearchInput(`${foundRepository.owner.login} - ${foundRepository.name}`);
+		else setSearchInput('');
+	}, [selectedRepository]);
 
 	const cloneToBackend = async (repository: string) => {
 		try {
@@ -75,14 +77,19 @@ const GithubModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void
 
 	const getRepositories = async () => {
 		try {
-			const result = await axios.get(`/api/computing/github/repositories?page=${selectedPage}`);
-			if (result.status !== 200) throw new Error("Unable to load repositories from github's API");
-			setHasMore(result.data.hasMore);
-			setRepositories(result.data.repositories);
-			toast({
-				title: 'Repositories loaded',
-				status: 'success',
-			});
+			const repositoryList: GitHubRepository[] = [];
+			let hasMore = true;
+
+			setRepositoriesLoading(true);
+			for (let i = 1; hasMore; i += 1) {
+				// eslint-disable-next-line no-await-in-loop
+				const result = await axios.get(`/api/computing/github/repositories?page=${i}`);
+				if (result.status !== 200) throw new Error("Unable to load repositories from github's API");
+				hasMore = result.data.hasMore;
+				repositoryList.push(...result.data.repositories);
+			}
+			setRepositories(repositoryList);
+			setRepositoriesLoading(false);
 		} catch (error) {
 			console.error(error);
 		}
@@ -138,47 +145,50 @@ const GithubModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void
 				)}
 				{session && (
 					<>
-						<VStack spacing="5%">
+						<VStack spacing="16px" w="100%" align="start">
 							<CustomProgram
 								customName={customName}
 								setCustomName={setCustomName}
 								customEntrypoint={customEntrypoint}
 								setCustomEntrypoint={setCustomEntrypoint}
 							/>
-							<VStack w="100%">
-								<HStack justify="space-between" w="100%">
-									<Text>Dont see your repository?</Text>
-									<HStack spacing="16px">
-										<ChevronLeftIcon
-											cursor={selectedPage > 1 ? 'pointer' : 'not-allowed'}
-											onClick={() => setSelectedPage(selectedPage === 1 ? 1 : selectedPage - 1)}
-										/>
-										<Text>{selectedPage}</Text>
-										<ChevronRightIcon
-											cursor={hasMore ? 'pointer' : 'not-allowed'}
-											onClick={() => setSelectedPage(hasMore ? selectedPage + 1 : selectedPage)}
-										/>
+							<VStack spacing="8px" align="start" w="100%">
+								<VStack spacing="8px" align="start" w="100%">
+									<HStack>
+										<Text size="boldLg">The name of the repository</Text>
+										<Tooltip text="You need to select a repository from the list below, to deploy your program." />
 									</HStack>
-								</HStack>
-								<Select
-									onChange={(e: ChangeEvent<HTMLSelectElement>) => setSelectedRepository(e.target.value)}
-									placeholder="Select repository"
-								>
-									{repositories.map((repository, index: number) => (
-										<option key={index} value={repository.html_url}>
-											{repository.owner.login} - {repository.name}
-										</option>
-									))}
-								</Select>
+									<Input
+										value={searchInput}
+										onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value)}
+									/>
+								</VStack>
+								<Card w="100%" p="8px">
+									<Skeleton isLoaded={!repositoriesLoading} w="100%" borderRadius="8px">
+										<VStack h="200px" overflowY="scroll" align="start" spacing="4px" pr="8px" w="100%">
+											{repositories
+												.filter((repository) =>
+													`${repository.owner.login.toLowerCase()} - ${repository.name.toLowerCase()}`.includes(
+														searchInput.toLowerCase(),
+													),
+												)
+												.map((repository) => (
+													<Text
+														p="4px 8px"
+														borderRadius="8px"
+														w="100%"
+														_hover={{
+															bg: 'blue.100',
+														}}
+														onClick={() => setSelectedRepository(repository.html_url)}
+													>
+														{repository.owner.login} - {repository.name}
+													</Text>
+												))}
+										</VStack>
+									</Skeleton>
+								</Card>
 							</VStack>
-							<Button
-								variant="inline"
-								w="100%"
-								onClick={async () => signOut()}
-								id="ipc-dashboard-github-signout-button"
-							>
-								Sign out
-							</Button>
 						</VStack>
 					</>
 				)}
