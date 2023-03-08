29708ec624245459eae9dcdd4d46f446cb0cb01e
Lucas LOUIS <lucas.louis@epitech.eu>
2023-03-08 22:53:13 +0100

    feat: remove input placeholder & add title for each inputs

--- src/components/computing/CustomProgram.tsx
+++ src/components/computing/CustomProgram.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@chakra-ui/react';
+import { Input, Text, VStack } from '@chakra-ui/react';
 import { ChangeEvent } from 'react';
 
 type CustomProgramParams = {
@@ -14,18 +14,19 @@ const CustomProgram = ({
 	customEntrypoint,
 	setCustomEntrypoint,
 }: CustomProgramParams): JSX.Element => (
-	<>
-		<Input
-			placeholder="[Optional] Program name"
-			value={customName}
-			onChange={(e: ChangeEvent<HTMLInputElement>) => setCustomName(e.target.value)}
-		/>
-		<Input
-			placeholder="[Optional] Program entrypoint"
-			value={customEntrypoint}
-			onChange={(e: ChangeEvent<HTMLInputElement>) => setCustomEntrypoint(e.target.value)}
-		/>
-	</>
+	<VStack spacing="16px" w="100%">
+		<VStack spacing="8px" align="start" w="100%">
+			<Text size="boldLg">The name of the program</Text>
+			<Input value={customName} onChange={(e: ChangeEvent<HTMLInputElement>) => setCustomName(e.target.value)} />
+		</VStack>
+		<VStack spacing="8px" align="start" w="100%">
+			<Text size="boldLg">The entrypoint of the program</Text>
+			<Input
+				value={customEntrypoint}
+				onChange={(e: ChangeEvent<HTMLInputElement>) => setCustomEntrypoint(e.target.value)}
+			/>
+		</VStack>
+	</VStack>
 );
 
 export default CustomProgram;
