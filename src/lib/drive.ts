00848892c20730982498574f18a82e02ca0348b7
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-04 00:10:13 +0200

    feat: add programs to drive class constructor

--- src/lib/drive.ts
+++ src/lib/drive.ts
@@ -7,11 +7,13 @@ import fileDownload from 'js-file-download';
 
 import { ALEPH_CHANNEL } from 'config/constants';
 
-import type { AggregateType, IPCContact, IPCFile, IPCFolder, ResponseType, UploadResponse } from 'types/types';
+import type { AggregateType, IPCContact, IPCFile, IPCFolder, ResponseType, UploadResponse , IPCProgram} from 'types/types';
 
 export const MONTH_MILLIS = 86400 * 30 * 1000;
 
 class Drive {
+	public programs: IPCProgram[];
+
 	public files: IPCFile[];
 
 	public folders: IPCFolder[];
@@ -25,6 +27,7 @@ class Drive {
 		this.folders = [];
 		this.sharedFiles = [];
 		this.account = importedAccount;
+		this.programs = [];
 	}
 
 	public async loadShared(contacts: IPCContact[]): Promise<ResponseType> {
