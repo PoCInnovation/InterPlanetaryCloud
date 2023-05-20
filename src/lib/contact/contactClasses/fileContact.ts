6baab969a238852d857f939309475b3c00dafd9f
SEPHORAHANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2023-05-20 21:07:36 +0200

    refactor: updateFileContent

--- src/lib/contact/contactClasses/fileContact.ts
+++ src/lib/contact/contactClasses/fileContact.ts
@@ -53,6 +53,87 @@ class ContactFile extends Contact {
 			return { success: false, message: 'Failed to update this filename' };
 		}
 	}
+
+    public async getFileOwner(fileId: string): Promise<string | undefined> {
+        console.log(this.contacts);
+		let owner;
+		await Promise.all(
+			this.contacts.map(async (contact) => {
+				const aggr = await aggregate.Get<AggregateType>({
+					address: contact.address,
+					keys: ['InterPlanetaryCloud'],
+				});
+				const myContact = aggr.InterPlanetaryCloud.contacts.find((c) => c.address === this.account.address);
+				if (myContact?.files.find((f) => f.id === fileId)) {
+					owner = contact.publicKey;
+				}
+			}),
+		);
+        console.log("ALLALAL");
+		return owner;
+	}
+
+    public async updateFileContent(newFile: IPCFile): Promise<ResponseType> {
+		try {
+			let fileFound = false;
+			await Promise.all(
+				this.contacts.map(async (contact) => {
+					const file = contact.files.find((f) => f.id === newFile.id);
+
+					if (file && this.account) {
+						file.hash = newFile.hash;
+						file.encryptInfos = {
+							key: (
+								await this.account.encrypt(
+									await this.account.decrypt(Buffer.from(newFile.encryptInfos.key, 'hex')),
+									contact.publicKey,
+								)
+							).toString('hex'),
+							iv: (
+								await this.account.encrypt(
+									await this.account.decrypt(Buffer.from(newFile.encryptInfos.iv, 'hex')),
+									contact.publicKey,
+								)
+							).toString('hex'),
+						};
+						fileFound = true;
+						await this.publishAggregate();
+					}
+				}),
+			);
+
+			if (!fileFound && this.account) {
+				const owner = await this.getFileOwner(newFile.id);
+
+				if (!owner) {
+					return { success: false, message: 'File not found' };
+				}
+				// const fileKey = (
+				// 	await this.account.encrypt(await this.account.decrypt(Buffer.from(newFile.encryptKey, 'hex')))
+				// ).toString('hex');
+
+				const encryptInfos = {
+					key: (
+						await this.account.encrypt(await this.account.decrypt(Buffer.from(newFile.encryptInfos.key, 'hex')), owner)
+					).toString('hex'),
+					iv: (
+						await this.account.encrypt(await this.account.decrypt(Buffer.from(newFile.encryptInfos.iv, 'hex')), owner)
+					).toString('hex'),
+				};
+				await post.Publish({
+					account: this.account,
+					postType: 'InterPlanetaryCloud',
+					content: { file: { ...newFile, encryptInfos }, tags: ['update', newFile.id] },
+					channel: 'TEST',
+					storageEngine: ItemType.ipfs,
+				});
+			}
+			return { success: true, message: 'File content updated' };
+		} catch (err) {
+			console.error(err);
+			return { success: false, message: 'Failed to update the file content' };
+		}
+	}
 }
 
 export default ContactFile;
