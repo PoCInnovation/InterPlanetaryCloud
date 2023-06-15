1c3fb42a07c42807c7129a6d9ef4a3737ffcc554
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-06-15 22:15:31 +0200

    feat(sharing): added visualization of the programs that have been shared

--- pages/drive/shared.tsx
+++ pages/drive/shared.tsx
@@ -8,12 +8,16 @@ import { useDriveContext } from 'contexts/drive';
 
 const Shared = (): JSX.Element => {
 	const { sharedFiles } = useDriveContext();
+	const { sharedPrograms } = useDriveContext();
 
 	return (
 		<Navigation>
 			<VStack w="100%" spacing="48px" align="start">
 				<LabelBadge label="Share with me" />
-				<DriveCards files={sharedFiles.filter((elem) => !elem.deletedAt)} />
+				<DriveCards
+  				files={sharedFiles.filter((elem) => !elem.deletedAt)}
+  				programs={sharedPrograms}
+				/>
 			</VStack>
 		</Navigation>
 	);
